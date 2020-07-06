4f71f90e3c27c79c9feee7de95c69ffd7ba8bab9
M Halid Kutsal <mhalidkutsal@gmail.com>
2020-07-06 03:43:10 +0200

    initial commit

--- src/Componentss/Gamein.jsx
+++ src/Componentss/Gamein.jsx
@@ -609,7 +609,7 @@ class Gamein extends Component {
 
 
 
-        const API_URL = "http://localhost:5000/score"
+        const API_URL = window.location.hostname === "localhost" ? "http://localhost:5000/score" : "https://halidsgame.vercel.app/score"
 
 
 
